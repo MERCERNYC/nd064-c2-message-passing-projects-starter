99b236dcc2d2a491f66ec7738101dcf8457c3df8
Juliana Fogg <julianafogg@gmail.comgit config --global user.name Juliana>
2021-10-11 15:37:29 -0400

    add yamls files

---
+++ modules/kafka/producer.py
@@ -0,0 +1,5 @@
+from kafka import KafkaProducer
+producer = KafkaProducer(bootstrap_servers=' my-release-kafka-0.my-release-kafka-headless.default.svc.cluster.local:9092')
+msg1=dict({'first_name':'Juliana','last_name':'Lola','company_name':'Udacity'})
+producer.send('test', bytes(str(msg1), 'utf-8'))
+producer.flush()
