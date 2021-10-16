e616b882d780b3cbd5a299282ab295671d710ce8
Juliana Fogg <julianafogg@gmail.comgit config --global user.name Juliana>
2021-10-16 16:32:32 -0400

    fix deployment kafka-consumer

--- modules/kafka/producer.py
+++ modules/kafka/producer.py
@@ -1,5 +1,5 @@
 from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers=' my-release-kafka-0.my-release-kafka-headless.default.svc.cluster.local:9092')
+producer = KafkaProducer(bootstrap_servers=' my-release-kafka-0.my-release-kafka-headless.default.svc.cluster.local:9092, api_version=(2, 0, 2)')
 msg1=dict({'first_name':'Juliana','last_name':'Lola','company_name':'Udacity'})
 producer.send('test', bytes(str(msg1), 'utf-8'))
 producer.flush()
